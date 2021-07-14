cf88997f80f5881311b2e441421f3a9782a7bc21
peter_alfie <alfie1104@gmail.com>
2021-07-15 08:17:02 +0900

    Making earth rotate around itself day and night shift

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three";
 
@@ -6,7 +6,7 @@ import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 
 export function Earth(props) {
@@ -14,6 +14,15 @@ export function Earth(props) {
     TextureLoader,
     [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
   );
+
+  const earthRef = useRef();
+  const cloudsRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    earthRef.current.rotation.y = elapsedTime / 6;
+    cloudsRef.current.rotation.y = elapsedTime / 6;
+  });
 
   return (
     <>
@@ -27,7 +36,7 @@ export function Earth(props) {
         saturation={0}
         fade={true}
       />
-      <mesh>
+      <mesh ref={cloudsRef}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -37,7 +46,7 @@ export function Earth(props) {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh>
+      <mesh ref={earthRef}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
