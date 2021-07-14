ceae7676ca4c26d8c2fae29032c0f50880c440bd
peter_alfie <alfie1104@gmail.com>
2021-07-15 08:01:03 +0900

    Adding sun light and making earth look more real to properly reflect light

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -17,17 +17,18 @@ export function Earth(props) {
 
   return (
     <>
-      <ambientLight intensity={1} />
+      {/*<ambientLight intensity={1} />*/}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={20000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
       <mesh>
         <sphereGeometry args={[1.005, 32, 32]} />
-        <Stars
-          radius={300}
-          depth={60}
-          count={20000}
-          factor={7}
-          saturation={0}
-          fade={true}
-        />
         <meshPhongMaterial
           map={cloudsMap}
           opacity={0.4}
@@ -39,7 +40,12 @@ export function Earth(props) {
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
         <OrbitControls
           enableZoom={true}
           enablePan={true}
